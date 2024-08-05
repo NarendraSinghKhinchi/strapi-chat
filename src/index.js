b4bcc1fc924ea57cacc0197d1662508b1eae6941
Narenda Singh Khinchi <narendrasingh30324@gmail.com>
2024-08-06 04:33:51 +0530

    update api url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ module.exports = {
   bootstrap(/* { strapi } */) {
     const io = new Server(strapi.server.httpServer, {
       cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-app-one-lyart.vercel.app",
         methods: ["GET", "POST"],
         allowedHeaders: ["Authorization", "my-custom-header"],
         credentials: true,
